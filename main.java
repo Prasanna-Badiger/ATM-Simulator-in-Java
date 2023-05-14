0cd861a9745910a4cfd47c71cb2b8d7ff4d19d58
Prasanna-Badiger <prasannabindi123@gmail.com>
2023-05-14 22:27:16 +0530

    Replaced whole file with proper extension

---
+++ main.java
@@ -0,0 +1,10 @@
+package p1;
+import java.io.IOException;
+public class main extends options {
+	public static void main(String []args)throws Exception
+	{
+		options op=new options();
+		op.getlogin();
+	}
+
+}
